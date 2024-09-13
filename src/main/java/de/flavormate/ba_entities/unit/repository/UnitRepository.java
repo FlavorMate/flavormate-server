aa639a3329fc9d7d2ce41c0dfc84afc538938ec5
Chip3211 <github@thenus.de>
2024-09-13 23:06:43 +0300

    - Added script to clean unused units

--- src/main/java/de/flavormate/ba_entities/unit/repository/UnitRepository.java
+++ src/main/java/de/flavormate/ba_entities/unit/repository/UnitRepository.java
@@ -2,10 +2,15 @@ package de.flavormate.ba_entities.unit.repository;
 
 import de.flavormate.ba_entities.unit.model.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UnitRepository extends JpaRepository<Unit, Long> {
 
 	Optional<Unit> findByLabel(String label);
+
+	@Query(nativeQuery = true, value = "SELECT u.* FROM units u LEFT JOIN ingredients i ON u.id = i.unit_id WHERE i.unit_id IS NULL")
+	List<Unit> findEmpty();
 }
