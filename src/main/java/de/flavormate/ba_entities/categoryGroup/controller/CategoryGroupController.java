0f5b786731fe5e106663fe521b17734bb66ba96b
Chip3211 <github@thenus.de>
2024-09-14 20:32:11 +0300

    - Fixed permission access

--- src/main/java/de/flavormate/ba_entities/categoryGroup/controller/CategoryGroupController.java
+++ src/main/java/de/flavormate/ba_entities/categoryGroup/controller/CategoryGroupController.java
@@ -3,7 +3,6 @@ package de.flavormate.ba_entities.categoryGroup.controller;
 import de.flavormate.ab_exeptions.exceptions.CustomException;
 import de.flavormate.ba_entities.categoryGroup.model.CategoryGroup;
 import de.flavormate.ba_entities.categoryGroup.service.CategoryGroupService;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v2/categoryGroups")
-@Secured({"ROLE_ADMIN"})
 public class CategoryGroupController {
 
 	private final CategoryGroupService service;
