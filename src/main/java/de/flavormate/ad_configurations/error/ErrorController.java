e6691f1c479b9a33e39bb86866ee627602f785c9
Chip3211 <github@thenus.de>
2024-09-30 10:59:18 +0200

    - Add new icon to static resources - Update error.html image and link URLs

--- src/main/java/de/flavormate/ad_configurations/error/ErrorController.java
+++ src/main/java/de/flavormate/ad_configurations/error/ErrorController.java
@@ -1,21 +1,23 @@
 package de.flavormate.ad_configurations.error;
 
 import de.flavormate.ad_configurations.FlavorMateConfig;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Controller
-@RequiredArgsConstructor
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-	private final SpringTemplateEngine templateEngine;
-	private final FlavorMateConfig flavorMateConfig;
 
+	/**
+	 * Handles error requests by populating the model with necessary URLs.
+	 *
+	 * @param model the model to which the frontend and backend URLs will be added
+	 * @return the view name to be rendered, in this case, "error"
+	 */
 	@RequestMapping("/error")
 	public String handleError(Model model) {
-		model.addAttribute("baseUrl", FlavorMateConfig.getFrontendUrl());
+		model.addAttribute("frontendUrl", FlavorMateConfig.getFrontendUrl());
+		model.addAttribute("backendUrl", FlavorMateConfig.getBackendUrl());
 		return "error";
 	}
 }
