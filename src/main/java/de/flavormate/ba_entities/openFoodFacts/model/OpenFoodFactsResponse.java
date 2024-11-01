8634203904473158b90c81a9de07138ee70ddc5c
Chip3211 <github@thenus.de>
2024-11-01 20:28:46 +0100

    - Add Javadoc to OpenFoodFactsResponse class

--- src/main/java/de/flavormate/ba_entities/openFoodFacts/model/OpenFoodFactsResponse.java
+++ src/main/java/de/flavormate/ba_entities/openFoodFacts/model/OpenFoodFactsResponse.java
@@ -1,5 +1,14 @@
 package de.flavormate.ba_entities.openFoodFacts.model;
 
+/**
+ * Represents a response from the OpenFoodFacts API.
+ *
+ * @param product        The product information returned by the OpenFoodFacts API.
+ * @param status         The status code of the response.
+ * @param status_verbose A verbose description of the status.
+ * @param code           The barcode of the product.
+ */
 public record OpenFoodFactsResponse(OpenFoodFactsProduct product, int status, String status_verbose,
 									String code) {
 }
+
