3f92a2f6e148934f936f2a973d5c80b7251c1f67
Chip3211 <github@thenus.de>
2024-11-01 20:50:32 +0100

    - Add Javadoc for OpenFoodFactsProduct

--- src/main/java/de/flavormate/ba_entities/openFoodFacts/model/OpenFoodFactsProduct.java
+++ src/main/java/de/flavormate/ba_entities/openFoodFacts/model/OpenFoodFactsProduct.java
@@ -1,4 +1,9 @@
+/* Licensed unter AGPLv3 2024 */
 package de.flavormate.ba_entities.openFoodFacts.model;
 
-public record OpenFoodFactsProduct(OpenFoodFactsNutriments nutriments) {
-}
+/**
+ * Represents a product retrieved from the OpenFoodFacts API.
+ *
+ * @param nutriments The nutritional information of the product.
+ */
+public record OpenFoodFactsProduct(OpenFoodFactsNutriments nutriments) {}
