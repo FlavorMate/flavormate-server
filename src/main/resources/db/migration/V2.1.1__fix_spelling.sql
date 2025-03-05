e0c994d9c7c1d6665d070bb10c438549480c5234
Chip3211 <github@thenus.de>
2025-03-05 10:49:50 +0100

    - Fixed spelling for categories

---
+++ src/main/resources/db/migration/V2.1.1__fix_spelling.sql
@@ -0,0 +1,14 @@
+UPDATE public.category_i18n
+SET value = 'Backen'
+WHERE category_id = 19
+  AND language = 'de';
+
+UPDATE public.category_i18n
+SET value = 'Haltbarmachen'
+WHERE category_id = 34
+  AND language = 'de';
+
+UPDATE public.category_i18n
+SET value = 'Käse'
+WHERE category_id = 36
+  AND language = 'de';
