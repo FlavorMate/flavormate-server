e08d71e7dc108371debf2b7987822019f1694d9c
Chip3211 <github@thenus.de>
2025-12-06 20:54:37 +0100

    - fixed url parsing bug

--- src/main/kotlin/de/flavormate/extensions/scraper/controllers/ScraperController.kt
+++ src/main/kotlin/de/flavormate/extensions/scraper/controllers/ScraperController.kt
@@ -7,6 +7,7 @@ import jakarta.annotation.security.RolesAllowed
 import jakarta.enterprise.context.RequestScoped
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
+import java.net.URLDecoder
 import java.util.*
 
 @RequestScoped
@@ -16,5 +17,10 @@ class ScraperController(val service: ScraperService) {
 
   @GET
   @Path("/{base64}")
-  fun scrape(base64: String) = service.scrape(Base64.getDecoder().decode(base64).decodeToString())
+  fun scrape(base64: String): String {
+    val uri = Base64.getDecoder().decode(base64).decodeToString()
+    val url = URLDecoder.decode(uri, Charsets.UTF_8)
+
+    return service.scrape(url = url)
+  }
 }
