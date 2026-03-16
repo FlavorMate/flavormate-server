3842948104ca56a07ce159c6fab1d376b0328ea7
Chip3211 <github@thenus.de>
2026-03-16 15:27:40 +0100

    - modified comments

--- src/main/kotlin/org/schema/services/LDJsonInstructionService.kt
+++ src/main/kotlin/org/schema/services/LDJsonInstructionService.kt
@@ -17,8 +17,7 @@ class LDJsonInstructionService {
    * The mapping logic considers whether the input steps contain groups of instructions or represent
    * a single or multiple instruction groups.
    *
-   * @param input A list of [org.schema.models.types.step.LDJsonStep] objects that represent the
-   *   input steps to be mapped.
+   * @param input A list of [LDJsonStep] objects that represent the input steps to be mapped.
    * @return A set of [RecipeDraftInstructionGroupEntity] objects created from the input steps.
    */
   fun mapInstructionGroupDrafts(
@@ -38,25 +37,21 @@ class LDJsonInstructionService {
   }
 
   /**
-   * Checks if the given list of [org.schema.models.types.step.LDJsonStep] contains any elements of
-   * type [org.schema.models.types.step.LDJsonHowToSection].
+   * Checks if the given list of [LDJsonStep] contains any elements of type [LDJsonHowToSection].
    *
-   * @param input A list of [org.schema.models.types.step.LDJsonStep] objects to be checked.
-   * @return True if the list contains at least one
-   *   [org.schema.models.types.step.LDJsonHowToSection]; otherwise, false.
+   * @param input A list of [LDJsonStep] objects to be checked.
+   * @return True if the list contains at least one [LDJsonHowToSection]; otherwise, false.
    */
   private fun containsGroups(input: List<LDJsonStep>): Boolean =
     input.any { it is LDJsonHowToSection }
 
   /**
-   * Checks whether the provided list of [org.schema.models.types.step.LDJsonStep] contains any
-   * objects that represent single instruction groups. This is determined by verifying if the
-   * element is either an [org.schema.models.types.step.LDJsonHowToStep] or an
-   * [org.schema.models.types.step.LDJsonHowToSection] containing at most one instructional element.
+   * Checks whether the provided list of [LDJsonStep] contains any objects that represent single
+   * instruction groups. This is determined by verifying if the element is either an
+   * [LDJsonHowToStep] or an [LDJsonHowToSection] containing at most one instructional element.
    *
-   * @param input A list of [org.schema.models.types.step.LDJsonStep] objects to evaluate. This may
-   *   include instances of [org.schema.models.types.step.LDJsonHowToStep] and
-   *   [org.schema.models.types.step.LDJsonHowToSection].
+   * @param input A list of [LDJsonStep] objects to evaluate. This may include instances of
+   *   [LDJsonHowToStep] and [LDJsonHowToSection].
    * @return True if any element in the list qualifies as a single instruction group; false
    *   otherwise.
    */
@@ -66,17 +61,14 @@ class LDJsonInstructionService {
     }
 
   /**
-   * Flattens a list of [org.schema.models.types.step.LDJsonStep] objects by checking if any of the
-   * steps are of type [org.schema.models.types.step.LDJsonHowToSection]. If an element is a
-   * [org.schema.models.types.step.LDJsonHowToSection], it creates a new section with a flattened
-   * list of its item elements. Otherwise, the Original step is retained.
+   * Flattens a list of [LDJsonStep] objects by checking if any of the steps are of type
+   * [LDJsonHowToSection]. If an element is a [LDJsonHowToSection], it creates a new section with a
+   * flattened list of its item elements. Otherwise, the Original step is retained.
    *
-   * @param input A list of [org.schema.models.types.step.LDJsonStep] objects to be flattened. This
-   *   list may contain instances of [org.schema.models.types.step.LDJsonHowToSection] that
-   *   themselves hold nested [org.schema.models.types.step.LDJsonStep] objects.
-   * @return A new list of [org.schema.models.types.step.LDJsonStep] objects where any
-   *   [org.schema.models.types.step.LDJsonHowToSection] instances have been replaced with a
-   *   flattened version, while other step types are kept unchanged.
+   * @param input A list of [LDJsonStep] objects to be flattened. This list may contain instances of
+   *   [LDJsonHowToSection] that themselves hold nested [LDJsonStep] objects.
+   * @return A new list of [LDJsonStep] objects where any [LDJsonHowToSection] instances have been
+   *   replaced with a flattened version, while other step types are kept unchanged.
    */
   private fun flattenGroups(input: List<LDJsonStep>): List<LDJsonStep> =
     input.map { step ->
@@ -90,8 +82,7 @@ class LDJsonInstructionService {
    * Creates a single instruction group from a list of steps. The resulting group contains all steps
    * as mapped instructions.
    *
-   * @param steps A list of [org.schema.models.types.step.LDJsonStep] objects to be converted into a
-   *   single instruction group.
+   * @param steps A list of [LDJsonStep] objects to be converted into a single instruction group.
    * @return A set containing one [RecipeDraftInstructionGroupEntity] with the mapped instructions.
    */
   private fun createSingleGroup(
@@ -106,12 +97,12 @@ class LDJsonInstructionService {
 
   /**
    * Creates a flattened instruction group from the provided list of steps. If the steps contain any
-   * groups (e.g., instances of [org.schema.models.types.step.LDJsonHowToSection]), their nested
-   * steps are flattened into individual instructions. All steps are then mapped and combined into a
-   * single instruction group.
+   * groups (e.g., instances of [LDJsonHowToSection]), their nested steps are flattened into
+   * individual instructions. All steps are then mapped and combined into a single instruction
+   * group.
    *
-   * @param steps A list of [org.schema.models.types.step.LDJsonStep] objects representing the steps
-   *   to be flattened and grouped into a single instruction group.
+   * @param steps A list of [LDJsonStep] objects representing the steps to be flattened and grouped
+   *   into a single instruction group.
    * @return A set containing one [RecipeDraftInstructionGroupEntity] with the flattened and mapped
    *   instructions derived from the provided steps.
    */
@@ -134,12 +125,11 @@ class LDJsonInstructionService {
 
   /**
    * Creates multiple instruction groups from a list of steps. Each group corresponds to a
-   * [org.schema.models.types.step.LDJsonHowToSection] within the input steps, with its name used as
-   * the group label and its elements mapped into instructions.
+   * [LDJsonHowToSection] within the input steps, with its name used as the group label and its
+   * elements mapped into instructions.
    *
-   * @param steps A list of [org.schema.models.types.step.LDJsonStep] objects, which only includes
-   *   [org.schema.models.types.step.LDJsonHowToSection] instances that represent groups of
-   *   instructions.
+   * @param steps A list of [LDJsonStep] objects, which only includes [LDJsonHowToSection] instances
+   *   that represent groups of instructions.
    * @return A set of [RecipeDraftInstructionGroupEntity] objects, where each entity represents a
    *   group of instructions derived from the input steps.
    */
@@ -159,11 +149,10 @@ class LDJsonInstructionService {
     }
 
   /**
-   * Maps an [org.schema.models.types.step.LDJsonHowToStep] object to an
-   * [RecipeDraftInstructionGroupItemEntity]. The mapping sets the `label` of the resulting entity
-   * to the `text` of the input step.
+   * Maps an [LDJsonHowToStep] object to an [RecipeDraftInstructionGroupItemEntity]. The mapping
+   * sets the `label` of the resulting entity to the `text` of the input step.
    *
-   * @param input The [org.schema.models.types.step.LDJsonHowToStep] object to be mapped.
+   * @param input The [LDJsonHowToStep] object to be mapped.
    * @return An [RecipeDraftInstructionGroupItemEntity] with its `label` property set to the `text`
    *   of the input.
    */
