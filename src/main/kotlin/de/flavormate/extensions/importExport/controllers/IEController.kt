ea0b4bb6f736ecd68cc29595d1d43adbc68065b8
Chip3211 <github@thenus.de>
2026-04-06 22:27:21 +0200

    - Add `IEService` to handle import/export operations - Add `IEController` for API endpoints handling import/export

---
+++ src/main/kotlin/de/flavormate/extensions/importExport/controllers/IEController.kt
@@ -0,0 +1,47 @@
+/* Licensed under AGPLv3 2024 - 2026 */
+package de.flavormate.extensions.importExport.controllers
+
+import de.flavormate.extensions.importExport.interfaces.IEPluginMetadata
+import de.flavormate.extensions.importExport.services.IEService
+import de.flavormate.features.role.enums.RoleTypes
+import jakarta.annotation.security.RolesAllowed
+import jakarta.enterprise.context.RequestScoped
+import jakarta.validation.constraints.NotBlank
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.Response
+import java.io.File
+import org.jboss.resteasy.reactive.RestForm
+import org.jboss.resteasy.reactive.RestPath
+
+@RequestScoped
+@RolesAllowed(RoleTypes.USER_VALUE)
+@Path("/v3/import-export")
+class IEController(private val service: IEService) {
+
+  @GET
+  @Path("/importers")
+  fun getAvailableImporters(): List<IEPluginMetadata> = service.getAvailableImporters()
+
+  @GET
+  @Path("/exporters")
+  fun getAvailableExporters(): List<IEPluginMetadata> = service.getAvailableExporters()
+
+  @POST
+  @Path("import/{pluginId}")
+  fun import(
+    @RestPath @NotBlank pluginId: String,
+    @RestForm("file") files: List<File>?,
+    @RestForm("url") urls: List<String>?,
+  ): List<String> = service.import(pluginId = pluginId, files = files, urls = urls)
+
+  @POST
+  @Produces("application/zip")
+  @Path("export/{pluginId}")
+  fun export(
+    @RestPath @NotBlank pluginId: String,
+    @RestForm("recipe") recipes: List<String>,
+  ): Response = service.export(pluginId = pluginId, recipes = recipes)
+}
---
+++ src/main/kotlin/de/flavormate/extensions/importExport/services/IEService.kt
@@ -0,0 +1,83 @@
+/* Licensed under AGPLv3 2024 - 2026 */
+package de.flavormate.extensions.importExport.services
+
+import de.flavormate.exceptions.FBadRequestException
+import de.flavormate.extensions.importExport.interfaces.IEPluginMetadata
+import de.flavormate.extensions.importExport.models.inputSource.FileInputSource
+import de.flavormate.extensions.importExport.models.inputSource.IEInputSource
+import de.flavormate.extensions.importExport.models.inputSource.UrlInputSource
+import de.flavormate.features.recipe.repositories.RecipeRepository
+import de.flavormate.shared.enums.Language
+import jakarta.enterprise.context.RequestScoped
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.core.StreamingOutput
+import java.io.File
+import java.nio.file.Files
+import org.apache.commons.io.FileUtils
+
+@RequestScoped
+class IEService(
+  private val iePluginManager: IEPluginManager,
+  private val ieRecipeDraftConvertService: IERecipeDraftConvertService,
+  private val ieRecipeConvertService: IERecipeConvertService,
+  private val recipeRepository: RecipeRepository,
+) {
+
+  fun getAvailableImporters(): List<IEPluginMetadata> {
+    val importers = iePluginManager.getImportPlugins()
+
+    return importers.map { it.metadata }
+  }
+
+  fun getAvailableExporters(): List<IEPluginMetadata> {
+    val exporters = iePluginManager.getExportPlugins()
+
+    return exporters.map { it.metadata }
+  }
+
+  fun import(pluginId: String, files: List<File>?, urls: List<String>?): List<String> {
+    val inputs = mutableListOf<IEInputSource>()
+
+    files?.forEach { inputs.add(FileInputSource(file = it)) }
+
+    urls?.forEach { inputs.add(UrlInputSource.fromString(it)) }
+
+    if (inputs.isEmpty()) throw FBadRequestException("No file or url provided")
+
+    val normalized = iePluginManager.import(pluginId, input = inputs)
+
+    val drafts =
+      normalized.map {
+        val draft = ieRecipeDraftConvertService.map(it)
+        draft.id
+      }
+
+    return drafts
+  }
+
+  fun export(pluginId: String, recipes: List<String>): Response {
+    if (recipes.isEmpty()) {
+      throw FBadRequestException("No recipes provided")
+    }
+
+    val workDirectory = Files.createDirectories(Files.createTempDirectory("ie-export-"))
+
+    val files =
+      recipes.mapNotNull {
+        val recipe = recipeRepository.findById(it) ?: return@mapNotNull null
+
+        ieRecipeConvertService.map(recipe, Language.EN)
+      }
+
+    val zipFile = iePluginManager.export(pluginId, workDirectory, files)
+
+    val stream = StreamingOutput { output ->
+      Files.newInputStream(zipFile).use { input -> input.copyTo(output) }
+      FileUtils.deleteDirectory(workDirectory.toFile())
+    }
+
+    return Response.ok(stream)
+      .header("Content-Disposition", """attachment; filename="${zipFile.fileName}"""")
+      .build()
+  }
+}
