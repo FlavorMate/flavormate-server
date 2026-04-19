25aca211103c43002a668b0698cb8fc3700fd721
Chip3211 <github@thenus.de>
2026-04-19 18:33:31 +0200

    - Override `toString` in `IERecipeServing` model

--- src/main/kotlin/de/flavormate/extensions/importExport/models/ieRecipe/IERecipeServing.kt
+++ src/main/kotlin/de/flavormate/extensions/importExport/models/ieRecipe/IERecipeServing.kt
@@ -1,4 +1,9 @@
 /* Licensed under AGPLv3 2024 - 2026 */
 package de.flavormate.extensions.importExport.models.ieRecipe
 
-data class IERecipeServing(val amount: Double, val label: String) {}
+data class IERecipeServing(val amount: Double, val label: String) {
+
+  override fun toString(): String {
+    return "$amount $label"
+  }
+}
